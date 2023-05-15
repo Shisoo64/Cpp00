ec3c6355d1359d70c43d99faad67d3907db93d5d
Romain Laforge <rlaforge@3F6.42angouleme.fr>
2023-05-15 15:01:47 +0200

    ok

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: rlaforge <rlaforge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 16:41:57 by rlaforge          #+#    #+#             */
-/*   Updated: 2023/04/24 14:47:46 by rlaforge         ###   ########.fr       */
+/*   Updated: 2023/05/15 14:59:18 by rlaforge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 int main(int ac, char **av)
 {
+	if (ac == 1)
+		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < ac; i++ )
 	{
 		std::string str = av[i];
